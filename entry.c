fe585b54e92f4316fe229ca8b9ca0f518866b0dc
Ishan Karmakar <ishan.karmakar24@gmail.com>
2023-04-10 20:41:47 -0500

    commit

--- entry.c
+++ entry.c
@@ -1,8 +1,8 @@
 #include <efi.h>
 #include <efilib.h>
 
-EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    InitializeLib(ImageHandle, SystemTable);
-    Print(L"Hello World\n");
-    return EFI_SUCCESS;
+EFI_STATUS
+efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab) {
+    EFI_STATUS status = uefi_call_wrapper(systab->ConOut->OutputString, 2, systab->ConOut, L"Hello World\n");
+    return status;
 }
