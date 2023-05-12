42a99c56372ecec96169958b390c8ec4a435f69f
Ishan Karmakar <ishan.karmakar24@gmail.com>
2023-05-12 05:22:47 -0500

    FINALLY GOT PAGING WORKING

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,20 +4,38 @@
 #include "gdt.h"
 #include "idt.h"
 #include "log.h"
+#define PAGE_TABLE_ENTRIES 512
+#define PAGE_SIZE 4096
 
 bootparam_t* bootp;
-uint64_t pml4[512] __attribute__((aligned(4096)));
-uint64_t pdpte[512] __attribute__((aligned(4096)));
-uint64_t pde[512] __attribute__((aligned(4096)));
-uint64_t pte[512] __attribute__((aligned(4096)));
 
-void initialize_page_tables(void) {
-    for (int i = 0; i < 512; i++) {
-        pml4[i] = 0x2;
-        pdpte[i] = 0x2;
-        pde[i] = 0x2;
-        pte[i] = 0x2;
-    }
+static inline void set_cr3(uint64_t cr3) {
+    asm volatile ("mov %0, %%cr3" : : "r" (cr3));
+}
+
+static inline uint64_t get_cr3(void) {
+    uint64_t ret;
+    asm volatile ("mov %%cr3, %0" : "=r" (ret));
+    return ret;
+}
+
+static inline void set_cr0(uint64_t cr0) {
+    asm volatile ("mov %0, %%cr0" : : "r" (cr0));
+}
+
+static inline uint64_t get_cr0(void) {
+    uint64_t ret;
+    asm volatile ("mov %%cr0, %0" : "=r" (ret));
+    return ret;
+}
+
+void init_paging(void) {
+    uint64_t* pml4 = (uint64_t*) 0x124000;
+    for (int i = 0; i < PAGE_TABLE_ENTRIES; i++)
+        pml4[i] = 0;
+
+    set_cr3((uint64_t) pml4);
+    set_cr0(get_cr0() | 0x80000000);
 }
 
 /**
@@ -29,6 +47,7 @@ void _start(bootparam_t *bootpar)
     init_screen();
     load_gdt();
     load_idt();
-    initialize_page_tables();
+    
+    // setup_page_tables();
     for (;;);
 }
