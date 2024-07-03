309424ebc868584f6da9a63f82b87ed08d341abf
Ishan Karmakar <ishan.karmakar24@gmail.com>
2024-07-03 14:56:47 -0500

    stuff

--- src/bios/boot1.asm
+++ src/bios/boot1.asm
@@ -1,33 +1,64 @@
 ; BIOS Bootloader stage 1
 ; BIOS will directly load this
+; FIXME: Try to find a way to do this dynamically
+%define BIOS2_SECTORS 1
 
 [bits 16]
 [org 0x7C00]
 
-mov si, entered_bl
+mov di, entered_bl
 call print_string
 
 mov sp, 0x8000
 mov bp, sp
 
-mov bx, 0x8000
-mov ah, 2
-mov al, 1
-mov cl, 1
-mov ch, 0
-mov dh, 0
-int 0x13
-jc derror
+call check_lba_ext
+call load_bios2
 jmp $
 
-derror:
-    mov si, disk_error
+load_bios2:
+    mov si, disk_addr_packet
+    mov ah, 0x42
+    mov dl, 0x80
+    mov di, disk_error
+    int 0x13
+    jc handle_error
+    ret
+
+check_lba_ext:
+    mov ah, 0x41
+    mov bx, 0x55AA
+    mov dl, 0x80
+    mov di, ext_lba_error
+    int 0x13
+    jc handle_error
+    mov al, 0xEB
+    cmp al, [0x8000]
+    jne handle_error
+    ret
+
+handle_error:
+    push di
+    mov di, error_msg
+    call print_string
+    pop di
     call print_string
     jmp $
 
 %include "screen.asm"
 
-entered_bl db 'Entered Bootloader', 0xA, 0xD, 0
-disk_error db 'Disk read error', 0
+disk_addr_packet:
+    db 16
+    db 0
+    dw BIOS2_SECTORS
+    dw 0
+    dw 0x8000
+    dd 35
+    dd 0
+
+error_msg db 'Error: ', 0
+entered_bl db 'Entered bootloader', 0xA, 0xD, 0
+ext_lba_error db '1', 0xA, 0xD, 0
+disk_error db '2', 0xA, 0xD, 0
 TIMES 510 - ($ - $$) db 0
 dw 0xAA55
