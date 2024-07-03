1515812c14cf1eebc560e4e1571b30615f551461
Ishan Karmakar <ishan.karmakar24@gmail.com>
2024-07-03 07:08:57 -0500

    Fixed another problem with CMake

--- src/bios/bootloader.asm
+++ src/bios/bootloader.asm
@@ -1,7 +1,27 @@
 [bits 16]
 [org 0x7C00]
 
+mov si, hello_string
+call print_string
 jmp $
 
+; AL contains char to print
+print_char:
+    mov ah, 0xE
+    mov bl, 7
+    mov bh, 0
+    int 0x10
+    ret
+
+print_string:
+    mov al, [si]
+    call print_char
+    inc si
+    or al, al
+    jnz print_string
+
+    ret
+
+hello_string db 'Hello World', 0
 TIMES 510 - ($ - $$) db 0
 dw 0xAA55
