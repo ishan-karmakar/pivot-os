061f00bf80ffe1587bdedb3a60d11d42d7d5c2d5
Ishan Karmakar <ishan.karmakar24@gmail.com>
2024-07-14 21:17:33 -0500

    Trying ACPI in bootloader

--- src/uefi/acpi.c
+++ src/uefi/acpi.c
@@ -14,7 +14,7 @@ static uintptr_t search_guid(const efi_guid_t *guid) {
     return 0;
 }
 
-struct [[gnu::packed]] rsdp {
+struct __attribute__((packed)) rsdp {
     char sig[8];
     uint8_t checksum;
     char oemid[6];
@@ -27,12 +27,51 @@ struct [[gnu::packed]] rsdp {
     uint8_t rsv[3];
 };
 
+struct __attribute__((packed)) sdt {
+    char sig[4];
+    uint32_t length;
+    uint8_t revision;
+    uint8_t checksum;
+    char oemid[6];
+    char oemtableid[10];
+    uint32_t oem_revision;
+    uint32_t creator_id;
+    uint32_t creator_revision;
+};
+
+struct __attribute__((packed)) xsdt {
+    struct sdt header;
+    uint64_t tables[0];
+};
+
+bool validate(char *buffer, uint32_t length) {
+    uint8_t sum = 0;
+    for (uint32_t i = 0; i < length; i++)
+        sum += buffer[i];
+    return sum == 0;
+}
+
 void parse_rsdp(uintptr_t rsdp_addr) {
     struct rsdp *rsdp = (struct rsdp*) rsdp_addr;
-    if (!memcmp(rsdp->sig, "RSD PTR ", 8))
+    if (memcmp(rsdp->sig, "RSD PTR ", 8))
         log(Warning, "ACPI", "RSDP signature did not match");
-    
-    log(Info, "")
+    if (!validate((char*) rsdp, sizeof(struct rsdp)))
+        log(Warning, "ACPI", "RSDP did not match checksum");
+    if (!validate((char*) rsdp, rsdp->length))
+        log(Warning, "ACPI", "XSDP did not match checksum");
+    struct xsdt *xsdt = (struct xsdt*) rsdp->xsdt_addr;
+    log(Verbose, "ACPI", "%x", xsdt->header.length);
+    // if (!validate((char*) xsdt, xsdt->header.length))
+    //     log(Warning, "ACPI", "XSDT did not match");
+    uint32_t num_entries = (xsdt->header.length - sizeof(struct sdt)) / sizeof(uint64_t);
+    for (uint32_t i = 0; i < num_entries; i++) {
+        struct sdt *table = (struct sdt*) xsdt->tables[i];
+        log(Verbose, "ACPI", "%x", table);
+        char test[5];
+        test[4] = 0;
+        memcpy(test, table->sig, 4);
+        log(Verbose, "ACPI", "%s", test);
+    }
     while(1);
 }
 
