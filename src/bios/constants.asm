526d420f9ad8c059c0929ba2d89b0a97720e8fd2
Ishan Karmakar <ishan.karmakar24@gmail.com>
2024-07-27 22:12:01 -0500

    Removed UEFI and BIOS directories

--- src/bios/constants.asm
+++
@@ -1,9 +0,0 @@
-; IMPORTANT: This file is not reloaded on change
-; because the build process itself edits BIOS2_SECTORS 2
-
-%define BIOS2_ORG 0x8300
-%define FAT_START 34
-%define TARGET_WIDTH 1024
-%define TARGET_HEIGHT 768
-%define TARGET_BPP 32
-%define TARGET_MEMORY_MODEL 6
