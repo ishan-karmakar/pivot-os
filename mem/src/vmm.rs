1f721cef97fd9e59143b9a31eae3174c60e728a6
Ishan Karmakar <ishan.karmakar24@gmail.com>
2024-09-18 10:44:45 -0500

    Switching to putting buddy allocator metadata in start instead of putting it with data

--- mem/src/vmm.rs
+++ mem/src/vmm.rs
@@ -1,6 +1,10 @@
+use x86_64::structures::paging::PageTableFlags;
+
+use crate::{mapper::KMAPPER, pmm::{MAX_ADDR, PMM}, virt_addr};
+
 struct Block {
     pub size: usize, // Always a power of two
-    pub sibling: *mut Block
+    pub sibling: Option<*mut Block>
 }
 
 pub struct VirtualMemoryManager<const MB: usize = 0x1000>(*mut Block);
@@ -13,14 +17,22 @@ impl<const MB: usize> VirtualMemoryManager<MB> {
         loop {
             let block_size = 2_usize.pow(size.ilog2());
             size -= block_size as usize;
+            KMAPPER.lock().as_mut().unwrap().map(PMM.lock().frame(), start, PageTableFlags::NO_EXECUTE | PageTableFlags::WRITABLE);
+            unsafe { (*cur_block).size = block_size };
             if size == 0 {
-                return Self(0 as *mut Block);
+                unsafe { (*cur_block).sibling = None };
+                return Self(root_block);
+            } else {
+                start += block_size;
+                unsafe { (*cur_block).sibling = Some(start as *mut _); }
+                cur_block = start as *mut _;
             }
-            start += block_size;
         }
     }
 }
 
 pub(crate) fn init() {
-    let vmm: VirtualMemoryManager<> = VirtualMemoryManager::new(0, 0x11000);
+    let bottom = *MAX_ADDR.get().unwrap();
+    let vmm: VirtualMemoryManager<> = VirtualMemoryManager::new(virt_addr(bottom), bottom);
+    log::info!("Initialized kernel virtual memory manager");
 }
