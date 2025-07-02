cd70695847b1d363f872eed337405c55b30511dd
Ishan Karmakar <ishan.karmakar24@gmail.com>
2025-07-01 20:43:28 -0500

    fix: Fixed most of the errors now - just linking

--- src/lwip/lwipopts.h
+++ src/lwip/lwipopts.h
@@ -1,3 +1,15 @@
 #pragma once
 
 #define NO_SYS 1
+#define LWIP_SOCKET 0
+#define LWIP_NETCONN 0
+#define LWIP_IPV6 0
+#define LWIP_TIMERS 0
+
+#define MEMCPY(dst, src, len)
+#define SMEMCPY(dst, src, len)
+#define SYS_ARCH_PROTECT(lev)
+#define SYS_ARCH_DECL_PROTECT(lev)
+#define SYS_ARCH_UNPROTECT(lev)
+
+typedef int sys_prot_t;
--- src/lwip/string.h
+++ src/lwip/string.h
@@ -1,1 +1,8 @@
 #pragma once
+#include <stddef.h>
+
+static void memset() {}
+static void *memmove(void *dest, const void *source, size_t num) { return NULL; }
+static size_t strlen(const char *str) { return 0; }
+
+static int strncmp(const char *str1, const char *str2, size_t num) { return 0; }
