51c5f28c84e58f76a19cf00ecd3e4ee4f33a3e5b
Ishan Karmakar <ishan.karmakar24@gmail.com>
2025-11-25 17:17:09 -0600

    feat: Migrate PCIe to new functions

--- src/drivers/pci/pcie.zig
+++ src/drivers/pci/pcie.zig
@@ -19,8 +19,12 @@ fn init() kernel.Task.Ret {
     const num_entries = (tbl.hdr.length - @sizeOf(uacpi.acpi_mcfg)) / @sizeOf(uacpi.acpi_mcfg_allocation);
     const groups: [*]const uacpi.acpi_mcfg_allocation = tbl.entries();
     segment_groups = groups[0..num_entries];
-    // pci.read_reg = read_reg;
-    // pci.write_reg = write_reg;
+    pci.read_reg8 = GenerateReadFunc(u8);
+    pci.read_reg16 = GenerateReadFunc(u16);
+    pci.read_reg32 = GenerateReadFunc(u32);
+    pci.write_reg8 = GenerateWriteFunc(u8);
+    pci.write_reg16 = GenerateWriteFunc(u16);
+    pci.write_reg32 = GenerateWriteFunc(u32);
 
     for (segment_groups) |seg| {
         for (0..CONFIG_SPACE_PAGES) |i| {
@@ -38,13 +42,28 @@ fn get_addr(base: usize, addr: uacpi.uacpi_pci_address, off: u13) usize {
     return base + ((bus << 20) | (device << 15) | (func << 12)) + off;
 }
 
-pub fn read_reg(addr: uacpi.uacpi_pci_address, off: u13) u32 {
-    for (segment_groups) |seg| {
-        if (seg.segment == addr.segment) return get_addr(seg.address, addr, off).*;
-    }
-    @panic("PCIe segment not found");
+fn GenerateReadFunc(T: type) pci.ReadFunc(T) {
+    return struct {
+        fn read_reg(addr: uacpi.uacpi_pci_address, off: u13) T {
+            for (segment_groups) |seg| {
+                if (seg.segment == addr.segment) return @as(*T, @ptrFromInt(get_addr(seg.address, addr, off))).*;
+            }
+            @panic("PCIe segment not found");
+        }
+    }.read_reg;
 }
 
+fn GenerateWriteFunc(T: type) pci.WriteFunc(T) {
+    return struct {
+        fn write_reg(addr: uacpi.uacpi_pci_address, off: u13, val: T) void {
+            for (segment_groups) |seg| {
+                if (seg.segment == addr.segment)
+                    @as(*T, @ptrFromInt(get_addr(seg.address, addr, off))).* = val;
+            }
+            @panic("PCIe segment not found");
+        }
+    }.write_reg;
+}
 pub fn write_reg(addr: uacpi.uacpi_pci_address, off: u13, val: anytype) void {
     for (segment_groups) |seg| {
         if (seg.segment == addr.segment)
